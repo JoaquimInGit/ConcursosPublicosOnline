ed75f1afd4bc5a826e9f218e9945314a767fcec4
JoaquimInGit <joaquimemtomas@gmail.com>
2021-07-10 19:39:02 +0100

    fix filter_contests e alertas

--- app/Helpers/FiltersLogic.php
+++ app/Helpers/FiltersLogic.php
@@ -280,5 +280,193 @@ class FiltersLogic
             }
         }
 
+
+    }
+
+    /**
+     * Envia notificações consoante os ultimos registos criados na tabela de relação entre os contests e os filters
+     */
+    public static function sendNotifications2()
+    {
+
+        //$time = Carbon::createFromFormat('H', )->toDateTimeString();
+       // $actualtime = Carbon::now()->hour(8)->minutes(0)->seconds(0)->milliseconds(0);
+        $actualtime = Carbon::now()->minutes(0)->seconds(0)->milliseconds(0);
+      // ddd($actualtime);
+        $firstscapetime = Carbon::now()->hour('7')->minutes(0)->seconds(0)->milliseconds(0);
+        $secondScrape = Carbon::now()->subDay()->hour('13')->minutes(0)->seconds(0)->milliseconds(0);
+       // ddd($secondScrape);
+        //dd($actualtime->eq($firstscapetime));
+
+
+
+
+
+
+        $entities = Entity::where('status', '>=', 1)->get();
+
+        //array para enviar os contests de cada entidade
+        $contestsArray = array();
+        //$filtersArray = array();
+
+        //por cada entidade
+        foreach ($entities as $entity) {
+            try {
+                //encontra filtros de uma dada entidade
+                $filtros = Filter::where('entity_id', $entity->id)->get();
+                //encontra o user de uma dada entidade
+                $user = User::where('id', $entity->user_id)->first();
+
+                //por cada filtro
+                foreach ($filtros as $filtro) {
+
+
+                    //se a hora da primeira notificação for maior que a segunda ou seja já passa das 8 horas
+                    if($actualtime->eq($firstscapetime)){
+
+                        //corre as 8
+                        //vai buscar os contestfilters das 14 da tarde do dia anterior as 8 da manha daquele dia
+
+                        $contests = ContestFilter::where([['filter_id', $filtro->id], ['created_at', '>', $secondScrape]])->get();
+                        //  $contests = ContestFilter::where([ ['created_at', '>', $secondScrape]])->get();
+
+                        //  ddd($contests);
+                    }else{
+                        //corre as 14
+                        //contest filter desde das 8 da manhã as 14 da tarde
+                        $contests = ContestFilter::where([['filter_id', $filtro->id], ['created_at', '>', $firstscapetime]])->get();
+                        //    $contests = ContestFilter::where([['created_at', '>', $firstscapetime]])->get();
+                        //  ddd($contests);
+                    }
+
+
+                    //se o resultado da query anterior não for null
+                    // if (!empty($contests)){
+                    if (sizeof($contests) != 0) {
+                        // ddd($contests);
+                        //$fil = Filter::where('id',$filtro->id)->first();
+                        //array_push($filtersArray, $fil);
+                        //por cada concurso
+                        foreach ($contests as $c) {
+                            //colocamos o concurso no array
+                            $cont = Contest::where('id', $c->contest_id)
+                                ->first();
+                            //ddd($cont);
+                            array_push($contestsArray, $cont);
+                        }
+                    }
+                }
+                //se o array não for null
+                if (!empty($contestsArray)) {
+                    //enviamos uma notifiação
+                    $user->notify(new FilterNotification($contestsArray));
+                    //colocamos o array vazio
+                    $contestsArray = array();
+                    $filtersArray = array();
+                }
+            }catch (Exception $e){
+                echo "ocorreu um erro: ".$e;
+            }
+
+    }
+    }
+
+    public static function applyFilter2()
+    {
+        $filters = Filter::where('filter_status', '>=', 1)->get()->toArray();
+        $filters = json_encode($filters);
+        $filters = json_decode($filters);
+        //   dd($filters);
+        //ddd($teste);
+        foreach ($filters as $filter) {
+            try {
+                // dd($filter->filter_name);
+                $contests = Contest::where('created_at', '>=', Carbon::now()->minute(0)->seconds(0)->toDateTimeString('minute'));
+
+                if (!empty($filter->description_words)) {
+                    //dd($filter->description_words);
+                    $keywords = explode(',', $filter->description_words);
+                    $keys = "";
+                    foreach ($keywords as $word) {
+                        $aux = explode('"value":"', $word);
+                        $keys = $keys . $aux[1];
+                    }
+                    $keys = str_replace('"}', ' ', $keys);
+                    $keys = str_replace(']', ' ', $keys);
+                    //dd($keys);
+                    $searchValues = preg_split('/\s+/', $keys, -1, PREG_SPLIT_NO_EMPTY);
+                   //dd($searchValues);
+                    $contests = $contests->where(function ($q) use ($searchValues) {
+                        foreach ($searchValues as $value) {
+                           // ddd($q);
+                            $q->orWhere('description', 'like', "%{$value}%")
+                                ->orWhere('pdf_content', 'like', "%{$value}%");
+                            // ->orWhere('cpv_description', 'like', "%{$value}%");
+                        }
+                    });
+//dd($contests);
+                }
+
+                if (!empty($filter->contest_entity)) {
+                    $contests = $contests->where('entity', 'LIKE', "%" . $filter->contest_entity . "%");
+                }
+
+                if (!empty($filter->district)) {
+                    $contests = $contests->where('pdf_content', 'LIKE', "%" . $filter->district . "%");
+                }
+                //dd($contests);
+                if (!empty($filter->min_price)) {
+                    $contests = $contests->where('price', '>=', $filter->min_price);
+                }
+                if (!empty($filter->max_price)) {
+                    $contests = $contests->where('price', '<=', $filter->max_price);
+                }
+                if (!empty($filter->cpv)) {
+                    $contests = $contests->where('cpv', $filter->cpv);
+                }
+                if (!empty($filter->type_act)) {
+                    // ddd($filter->type_act);
+                    $contests = $contests->where('type_act', $filter->type_act);
+                    //ddd($contests->toArray());
+                }
+                if (!empty($filter->type_model)) {
+                    //   ddd($filter->type_model);
+                    //  ddd($contests->toArray());
+                    //  ddd($filter->type_model);
+                    // ddd($contests->where('type_model',$filter->type_model));
+                    //  ddd($contests->toArray());
+                    //  if($contests->where('type_model', $filter->type_model)->get() != null){
+                    $contests = $contests->where('type_model', $filter->type_model);
+                    //}
+
+                }
+                if (!empty($filter->type_contract)) {
+                    $contests = $contests->where('type_contract', $filter->type_contract);
+                }
+                if ($contests != null) {
+                    $contests = $contests->get()->toArray();
+
+                    $contests = json_encode($contests);
+                    $contests = json_decode($contests);
+
+                    //Retira todos os contestFilters da DB
+                    $contestFilters = ContestFilter::where('filter_id', '=', $filter->id)->get();
+                    foreach ($contests as $contest) {
+                        //vai validar um registo do contest filters para evitar que se coloquem repetidos
+                        if($contestFilters->where('contest_id', '=', $contest->id)->count() == 0) {
+                            //dd($contest);
+                            ContestFilter::create([
+                                'contest_id' => $contest->id,
+                                'filter_id' => $filter->id,
+                                'date' => Carbon::now()
+                            ]);
+                        }
+                    }
+                }
+            }catch(Exception $e){
+                //TODO:fazer log da exceção
+                return "erro encontrado: ".$e->getMessage();
+            }
+        }
     }
 }
