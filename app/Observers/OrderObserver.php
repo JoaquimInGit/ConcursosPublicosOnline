c34b74dbe5a26a1c79b19b4b1b830584d83f65af
Ricardo Vieira <ricvi.ricardovieira@gmail.com>
2021-07-12 16:54:17 +0100

    test 2 bug OrderObserver

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -28,49 +28,11 @@ class OrderObserver
      */
     public function updated(Order $order)
     {
-        if(auth()->user()->cannot('manageApp')) {
-            $enddate = OrderItem::where('entity_id', $order->entity_id)
-                ->where('status', 2)
-                ->orderBy('end_date', 'desc')->first();
-            //ddd($order);
-            // $orderItem = OrderItem::where('order_id', $order->id)->first();
-            //ddd($orderItem->product_id == 1);
-            //se a data de fim de pagamento for anterior ao dia de hoje
-            //ddd($enddate->end_date > Carbon::today());
-            if ($enddate != null && $enddate->end_date > Carbon::today()) {
-                // ddd($enddate->end_date > Carbon::today());
-                if ($order->status == 2) {
-                    $orderItem = OrderItem::where('order_id', $order->id)->first();
-                    if ($orderItem->product_id == 1) {
-                        $orderItem->update(['start_date' => $enddate->end_date, 'end_date' => $enddate->end_date->addMonth(), 'status' => 2]);
-                    } elseif ($orderItem->product_id == 2) {
-                        $orderItem->update(['start_date' => $enddate->end_date, 'end_date' => $enddate->end_date->addQuarters(2), 'status' => 2]);
-                    } elseif ($orderItem->product_id == 3) {
-                        $orderItem->update(['start_date' => $enddate->end_date, 'end_date' => $enddate->end_date->addYear(), 'status' => 2]);
-                    }
-                    \Debugbar::ERROR();
-                }
-            } else {
-                //  ddd($enddate->end_date > Carbon::today());
-                if ($order->status == 2) {
-
-                    $orderItem = OrderItem::where('order_id', $order->id)->first();
-                    //ddd(($order->status == 2).' '.$orderItem);
-                    if ($orderItem->product_id == 1) {
-                        $orderItem->update(['start_date' => Carbon::today(), 'end_date' => Carbon::today()->addMonth(), 'status' => 2]);
-                        $entity = Entity::where('id', $order->entity_id)->first();
-                        $entity->update(['status' => 1]);
-                    } elseif ($orderItem->product_id == 2) {
-                        $orderItem->update(['start_date' => Carbon::today(), 'end_date' => Carbon::today()->addQuarters(2), 'status' => 2]);
-                        $entity = Entity::where('id', $order->entity_id)->first();
-                        $entity->update(['status' => 1]);
-                    } elseif ($orderItem->product_id == 3) {
-                        $orderItem->update(['start_date' => Carbon::today(), 'end_date' => Carbon::today()->addYear(), 'status' => 2]);
-                        $entity = Entity::where('id', $order->entity_id)->first();
-                        $entity->update(['status' => 1]);
-                    }
-                }
-
+        if(auth()->user()->cannot('manageApp') == null) {
+            Order::changeOnPayment($order);
+        }else{
+            if($order->wasChanged('status') && $order->status == 2){
+                Order::changeOnPayment($order);
             }
         }
 
